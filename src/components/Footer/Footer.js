10c97aa2023225a19a96a5043c82ce57e0872591
Dmitrii Savchenko <dsavax@gmail.com>
2018-06-12 13:39:12 +0300

    Footer and Navigation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
 
     return (
-      <div className="App">
-        My Footer
+      <div>
+            <footer className="footer is-primary">
+		      <div class= "container">
+		        <div className="columns">
+		          <div className="column">
+		            <p>Интересно как это будет выглядеть на русском языке.</p>
+		          </div>
+		          <div className="column has-text-right">
+		            <a className="icon" href="#"><i class="fa fa-facebook"></i></a>
+		            <a className="icon" href="#"><i class="fa fa-twitter"></i></a>
+		          </div>
+		        </div>
+		      </div>
+		    </footer>
       </div>
     );
  
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,40 @@ import './Header.css';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
+
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {isToggleOn: false};
+
+	  this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		this.setState(prevState => ({
+			isToggleOn: !prevState.isToggleOn
+		}));
+	}
+
   render() {
+
+  	let menuActive = this.state.isToggleOn ? 'is-active' : '';
+
     return (
 	    <nav className="navbar" role="navigation" aria-label="main navigation">
 	      <div className="container">
 	        <div className="navbar-brand">
 	          <a className="navbar-item">My Company</a>
 
-	          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-	            <span aria-hidden="true"></span>
-	            <span aria-hidden="true"></span>
-	            <span aria-hidden="true"></span>
+	          <a role="button" className={'navbar-burger '+menuActive} aria-label="menu" aria-expanded="false" onClick={this.handleClick}>
+	            <div className="navbar-end">
+		            <span aria-hidden="true"></span>
+		            <span aria-hidden="true"></span>
+		            <span aria-hidden="true"></span>
+		        </div>
 	          </a>
 
-	          <div className="navbar-menu">
+	          <div className={'navbar-menu' +menuActive}>
 	              <div className="navbar-end">
 
 	              <Link to="/" className="navbar-item">Home</Link>
