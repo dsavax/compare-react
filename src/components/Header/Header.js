405d7dc648958628fe08e43a22704068c703979d
Dmitrii Savchenko <dsavax@gmail.com>
2018-06-12 05:09:14 +0300

    Header and Routing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <div className="App">
-        My Header
-      </div>
+	    <nav className="navbar" role="navigation" aria-label="main navigation">
+	      <div className="container">
+	        <div className="navbar-brand">
+	          <a className="navbar-item">My Company</a>
+
+	          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+	            <span aria-hidden="true"></span>
+	            <span aria-hidden="true"></span>
+	            <span aria-hidden="true"></span>
+	          </a>
+
+	          <div className="navbar-menu">
+	              <div className="navbar-end">
+
+	              <Link to="/" className="navbar-item">Home</Link>
+				  <Link to="/faq" className="navbar-item">Features</Link>
+				  <Link to="/faq" className="navbar-item">About</Link>
+				  <Link to="/faq" className="navbar-item">FAQ</Link>
+
+	                <div className="navbar-item">
+	                  <p className="control">
+	                    <a className="button is-primary is-outlined">
+	                      <span className="icon">
+	                        <i className="fa fa-download"></i>
+	                      </span>
+	                      <span>Join Now</span>
+	                    </a>
+	                  </p>
+	                </div>
+	              </div>
+	          </div>
+	        </div>
+	      </div>
+	    </nav>
     );
   }
 }
